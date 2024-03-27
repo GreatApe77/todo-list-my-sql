5924421967ad520e591e094b6b4c6b0aa2289495
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-27 10:58:11 -0300

    update env variables

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,8 +2,12 @@ import "dotenv/config";
 import { z } from "zod";
 
 const EnvironmentSchema = z.object({
-    PORT:z.string().optional().default("8080"),
-})
+  PORT: z.string().optional().default("8080"),
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_PORT: z.string().optional(),
+  DB_NAME:z.string()
+});
 
-
-export const env = EnvironmentSchema.parse(process.env)
+export const env = EnvironmentSchema.parse(process.env);
